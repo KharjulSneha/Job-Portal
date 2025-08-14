f39b2ba89ac42369dfb44328424ce64470c7b3c3
KharjulSneha <kharjulsneha@gmail.com>
2025-08-14 18:01:49 +0530

    role exception handler

---
+++ job.portal/src/main/java/job/portal/exceptions/RoleValidationException.java
@@ -0,0 +1,7 @@
+package job.portal.exceptions;
+
+public class RoleValidationException extends RuntimeException {
+    public RoleValidationException(String message) {
+        super(message);
+    }
+}
